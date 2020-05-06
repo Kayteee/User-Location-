261c8cd4b854092b120a24786ff89c350dd06626
Kushagra Tripathi <51934770+Kayteee@users.noreply.github.com>
2020-05-06 23:37:21 +0530

    Update Spinner.js

--- Spinner.js
+++ Spinner.js
@@ -13,3 +13,51 @@ Spinner.defaultProps = {
 };
 
 export default Spinner;
+
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 60%;
+  margin:20px
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 4px;
+}
+
+</style>
+</head>
+<body> 
+
+<table>
+  <tr>
+    <td>Quote Submitted Date</td>
+    <td>Date()</td>
+    
+  </tr>
+  <tr>
+    <td>TSE Contact Name</td>
+    <td>abc</td>
+   
+  </tr>
+  <tr>
+    <td>TSE Contact Email</td>
+    <td>abc@example.com</td>
+    
+  </tr>
+  <tr>
+    <td>Requestor Name</td>
+    <td>xyz</td>
+    
+  </tr>
+  <tr>
+    <td>Requestor Contact Details</td>
+    <td>xyz@example.com</td>
+  </tr>
+</table>
+
+</body>
